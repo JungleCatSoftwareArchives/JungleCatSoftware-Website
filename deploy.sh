942f4057005a9245619b9c92135cdc154d49d4ab
Bryan Wyatt <brwyatt@gmail.com>
2012-10-31 23:53:26 -0500

    fixing file group on 'saved' files

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,20 @@ fi
 
 echo "Deploying site to $destination..."
 
+webuser="www-data"
+
 databaseconfig="protected/config/database.php"
 yiiruntime="protected/runtime"
 
 # this is an environment-unique file not in source control
 scp $destination/$databaseconfig ./$databaseconfig
+chmod :$webuser ./$databaseconfig
 
 # this is site-specific runtime logs and data. We don't want to wipe these and they aren't in sourcecontrol
 scp -r $destination/$yiiruntume ./$yiiruntime
+chmod -R :$webuser ./$yiiruntime
 
-rsync -rltv --delete-delay --delete-excluded --delay-updates --force --group="www-data" --chmod=ug+w,+rx --exclude-from=rsync-ignore ./ $destination
+rsync -rltv --delete-delay --delete-excluded --delay-updates --force --group --chmod=ug+w,+rx --exclude-from=rsync-ignore ./ $destination
 
 # Remove from repo
 rm -rf ./$databaseconfig
