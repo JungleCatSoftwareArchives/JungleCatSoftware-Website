d5cc96797a8756afc97424666395a0764905b79f
Bryan Wyatt <brwyatt@gmail.com>
2012-10-31 23:42:31 -0500

    deploy.sh should be good now. It should backup the runtime folder so logs aren't lost

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 if [ -z $1 ]
 then
-	destination="/DATA/htdocs/dev.junglecatsoftware.com/"
+	destination="/DATA/htdocs/dev.junglecatsoftware.com"
 else
 	destination=$1
 fi
@@ -8,11 +8,16 @@ fi
 echo "Deploying site to $destination..."
 
 databaseconfig="protected/config/database.php"
+yiiruntime="protected/runtime"
 
 # this is an environment-unique file not in source control
 scp $destination/$databaseconfig ./$databaseconfig
 
+# this is site-specific runtime logs and data. We don't want to wipe these and they aren't in sourcecontrol
+scp -r $destination/$yiiruntume ./$yiiruntime
+
 rsync -rltv --delete-delay --delete-excluded --delay-updates --force --group="www-data" --chmod=ug+w,+rx --exclude-from=rsync-ignore ./ $destination
 
 # Remove from repo
-rm ./$databaseconfig
+rm -rf ./$databaseconfig
+rm -rf ./$yiiruntime
