1a2497bde598b78e62d74018a33f6cec9f5c90d0
Bryan Wyatt <brwyatt@gmail.com>
2012-10-31 23:07:13 -0500

    Adding echo to script, and verbose rsync

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 if [ -z $1 ]
 then
-	destination="/dev/null/"
+	destination="/DATA/htdocs/dev.junglecatsoftware.com/"
 else
 	destination=$1
 fi
+
+echo "Deploying site to $destination..."
 
 databaseconfig="protected/config/database.php"
 
 # this is an environment-unique file not in source control
 scp $destination/$databaseconfig ./$databaseconfig
 
-rsync -rlt --delete-delay --delete-excluded --delay-updates --force --group="www-data" --chmod=ug+w,+rx --exclude-from=rsync-ignore ./ $destination
+rsync -rltv --delete-delay --delete-excluded --delay-updates --force --group="www-data" --chmod=ug+w,+rx --exclude-from=rsync-ignore ./ $destination
 
 # Remove from repo
 rm ./$databaseconfig
