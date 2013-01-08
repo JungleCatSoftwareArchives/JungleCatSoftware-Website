51cc9492ab1ef5da211b9aebd99f664c83b3df51
Bryan Wyatt <brwyatt@gmail.com>
2013-01-07 20:33:32 -0600

    Updating index page

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -2,15 +2,9 @@
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+<p>Jungle Cat Software is an Open Source Software (OSS) organization interested in writing programs people want to use and enjoy using.</p>
 
-<p>You may change the content of this page by modifying the following two files:</p>
+<h3>Quick Links</h3>
 <ul>
-	<li>View file: <tt><?php echo __FILE__; ?></tt></li>
-	<li>Layout file: <tt><?php echo $this->getLayoutFile('main'); ?></tt></li>
+	<li><a href="https://github.com/organizations/JungleCatSoftware">GitHub</a></li>
 </ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
