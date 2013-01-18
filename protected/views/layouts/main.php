b2a38693b9f0085cf73dd3ff5c160beac4a1d4d0
Bryan Wyatt <brwyatt@gmail.com>
2013-01-17 19:16:45 -0600

    Adding Google Analytics

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -16,6 +16,17 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-33472085-3']);
+		_gaq.push(['_trackPageview']);
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
