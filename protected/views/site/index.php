1497768afa8524cf600b5fffdcf1a53f9a58dd95
Bryan Wyatt <brwyatt@gmail.com>
2013-08-12 21:28:06 -0500

    Updating index page

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,8 +4,11 @@
 
 <p>Jungle Cat Software is an Open Source Software (OSS) organization interested in writing programs people want to use and enjoy using.</p>
 
+<p>Current projects are currently geared towards hobby coding, specifically BRGE (a game engine) and BadScience! (a game that uses the BRGE engine), with the only goal being for personal development. However, some projects have leaned more to the useful side, such as BitcoinAccess (a Bitcoin RPC frontend for Android) and CryptNet (a secure P2P network). At present, development on all projects remains slow due to work obligations and minimal outside interest/exposure.</p>
+
 <h3>Quick Links</h3>
 <ul>
 	<li><a href="https://trello.com/junglecatsoftware">Trello</a></li>
 	<li><a href="https://github.com/organizations/JungleCatSoftware">GitHub</a></li>
+	<li><a href="https://jenkins.brwyatt.net/">Jenkins Build Server</a></li>
 </ul>
