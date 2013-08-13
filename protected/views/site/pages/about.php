0bd2ed07eb6a05fcc2356c9f68cc09c8c3e7b141
Bryan Wyatt <brwyatt@gmail.com>
2013-08-12 21:31:02 -0500

    Updating about page

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -11,4 +11,5 @@ $this->breadcrumbs=array(
 <h2>Current Members</h2>
 
 <h3>Bryan Wyatt</h3>
-<p>Bryan Wyatt is the founder and lead maintainer and has a passion for Open Source and making the world a better place. He is currently employed as a DevOps and Infrastructure Engineer specializing in building and maintaining Development and Production Linux servers with Puppet.</p>
+<p><a href="http://brwyatt.net/">http://brwyatt.net/</a></p>
+<p>Bryan Wyatt is the founder and lead maintainer of Jungle Cat Software, and has a passion for Open Source Software and making the world a better place. He is currently employed as a DevOps/Infrastructure Engineer, specializing in building and maintaining Development and Production Linux servers with Puppet.</p>
