e92209f9cb2b1ea97a188c1078f995dd4de257fe
Bryan Wyatt <brwyatt@gmail.com>
2013-08-12 21:33:15 -0500

    I thought I removed this word before that commit

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,7 +4,7 @@
 
 <p>Jungle Cat Software is an Open Source Software (OSS) organization interested in writing programs people want to use and enjoy using.</p>
 
-<p>Current projects are currently geared towards hobby coding, specifically BRGE (a game engine) and BadScience! (a game that uses the BRGE engine), with the only goal being for personal development. However, some projects have leaned more to the useful side, such as BitcoinAccess (a Bitcoin RPC frontend for Android) and CryptNet (a secure P2P network). At present, development on all projects remains slow due to work obligations and minimal outside interest/exposure.</p>
+<p>Current projects are geared towards hobby coding, specifically BRGE (a game engine) and BadScience! (a game that uses the BRGE engine), with the only goal being for personal development. However, some projects have leaned more to the useful side, such as BitcoinAccess (a Bitcoin RPC frontend for Android) and CryptNet (a secure P2P network). At present, development on all projects remains slow due to work obligations and minimal outside interest/exposure.</p>
 
 <h3>Quick Links</h3>
 <ul>
